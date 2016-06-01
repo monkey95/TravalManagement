19040b54dd7fb30dc9f0257b6da4621d01dede15
monkey95 <hungvmgc00672@fpt.edu.vn>
2016-06-02 00:04:02 +0700

    update DB

--- LibraryManagement.sql
+++ LibraryManagement.sql
@@ -2,12 +2,12 @@ create database LibraryManagement
 use LibraryManagement
 
 create table Account(
-	Username varchar(50),
+	Username varchar(25),
 	[Password] varchar(20)
 )
 
 create table Book(
-	ID varchar(50) primary key,
+	ID varchar(7) primary key,
 	BookName varchar(50),
 	AuthorName varchar(50),
 	Publisher varchar(100),
@@ -15,16 +15,18 @@ create table Book(
 )
 
 create table BorrowList(
-	BorrowerID varchar(10) foreign key references Borrower(BorrowerID),
-    IDBook varchar(50) foreign key references Book(ID),
+	BorrowerID int foreign key references Borrower(BorrowerID),
+    IDBook varchar(7) foreign key references Book(ID),
 	BorrowDate datetime,
 	ReturnDate datetime
 )
 
 create table Borrower(
-	BorrowerID varchar(10) primary key,
+	BorrowerID int identity primary key,
 	BorrowerName varchar(50),
-	PhoneNumber varchar(15)
+	PhoneNumber varchar(15),
+	[Address] varchar(500),
+	Email varchar(100)
 )
 
 select TenSach, Tomtat, TenLoai, TenNXB, Tentacgia 
@@ -39,9 +41,7 @@ insert into Book values('BK01','Tieng Viet lop 1','Nguyen Trai','Kim Dong',0)
 insert into Book values('BK02','Day hoc tot toan 12','Mai Anh Hung','Bo giao duc va dao tao',0)
 insert into Book values('BK03','Than thoai Hy Lap','Le Ba Tran Phuong','Nha XB Tre',0)
 
-insert into Book values(3,'Tieng Viet lop 1',1,'Nguyen Trai',50,0)
-insert into Book values(2,'Day hoc tot toan 12',3,'Mai Anh Hung',20,0)
-insert into Book values(4,'Than thoai Hy Lap',2,'Le Ba Tran Phuong',100,0)
+insert into Borrower values ('Chu Tien Tai', '0123456789','Gia Lam - Ha Noi','tientai@gmail.com')
 
 select * from Book
 select * from Borrower
